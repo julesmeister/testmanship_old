46c851fe76729cbadb7cbe3fcfc93273d765d339
Jules Lee <jackconsolidated@gmail.com>
2024-11-17 04:15:58 +0800

    include components ui in vercel

--- components/auth-ui/OauthSignIn.tsx
+++ components/auth-ui/OauthSignIn.tsx
@@ -1,11 +1,11 @@
 'use client';
 
-import { Button } from '@/components/ui/Button';
-import { signInWithOAuth } from '@/utils/auth-helpers/client';
-import { type Provider } from '@supabase/supabase-js';
+import { Button } from "@/components/ui/button";
+import { signInWithOAuth } from "@/utils/auth-helpers/client";
+import { type Provider } from "@supabase/supabase-js";
 import { FcGoogle } from "react-icons/fc";
 import { useState } from 'react';
-import { Input } from '@/components/ui/Input';
+import { Input } from "@/components/ui/input";
 
 type OAuthProviders = {
   name: Provider;
